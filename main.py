4a1f5d6d7a58df3aae48390b5b5a279572d928bd
gabriel-zomignani <gabrielz.zomig@gmail.com>
2025-10-25 11:59:38 +0100

    new file:   README.md 	new file:   config.yaml 	new file:   data/input.csv 	new file:   data/invalid_orders.csv 	new file:   data/valid_orders.csv 	new file:   main.py 	new file:   pipeline/__pycache__/reader.cpython-313.pyc 	new file:   pipeline/__pycache__/validator.cpython-313.pyc 	new file:   pipeline/discount.py 	new file:   pipeline/reader.py 	new file:   pipeline/validator.py 	new file:   pipeline/writer.py 	new file:   requirements.txt 	new file:   utils/discount_strategy.py 	new file:   utils/file_factory.py 	new file:   utils/idempotency_store.py 	new file:   utils/retry_decorator.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import threading
+import queue
+import yaml
+
+from pipeline.reader import Reader
+from pipeline.validator import Validator
+from pipeline.discount import DiscountApplier
+from pipeline.writer import Writer
+
+def load_config():
+    with open('config.yaml', 'r') as f:
+        return yaml.safe_load(f)
+
+def main():
+    config = load_config()
+
+    # Filas entre as etapas
+    q_validate = queue.Queue()
+    q_discount = queue.Queue()
+    q_output = queue.Queue()
+    q_invalid = queue.Queue()
+
+    # Threads das etapas
+    reader = Reader(config, q_validate)
+    validator = Validator(q_validate, q_discount, q_invalid)
+    discount = DiscountApplier(config, q_discount, q_output)
+    writer = Writer(config, q_output, q_invalid)
+
+    # Inicia as threads
+    threads = [
+        threading.Thread(target=reader.run),
+        threading.Thread(target=validator.run),
+        threading.Thread(target=discount.run),
+        threading.Thread(target=writer.run)
+    ]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
+
+if __name__ == "__main__":
+    main()
